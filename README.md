439d350bd2308cd839c59ea6cc881822d5e7a891
Brian Howson <bkhowson@gmail.com>
2018-06-06 00:17:27 -0400

    apply

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-"# posh" 
+# posh
+
+This is a repository of my powershell script modules.  These are things I didn't write for work, in fact I'm not sure how useful they are other than as learning excercises.
+
+## get-dnsoverhttp-mt
+
+Key points of this:
+
+* Using DNS over HTTP, invoke-webrequest with json
+* Supprting google and cloudflare
+* Multi-threading and optimizations
+* CAA Statistics for Alexa top 1M
+
+## Using DNS over HTTP
+
+This started as a script I wrote to test DNS over HTTP, to experiment with Cloudflare's 1.1.1.1 service, which provides a DNS over HTTP interface.  I expanded it to support Google's interface, which is essentially the same except google supports more features (subnet, dnssec checkign disable, "ANY" type queries).
+
+* Cloudflare was initially not escaping CAA records (fixed by first wednesday)
+* Google's escaping seems incorrect, results in (malformed) records not returning correctly.
+
+## Google and Cloudflare API differences
+
+Mostly both are the same, but there are some subtle differences.  My script defaults to cloudflare, but you can choose google with the -google switch, or by asking a question that cloudflare wouldn't answer.
+
+Field | Required? | Description | Example
+--- | --- | --- | ---
+name | Required | Query Name | example.com
+type | Required | Query Type (either a numeric value or text) | AAAA
+cd* | default: false | The CD (checking disabled) bit. | false
+edns_client_subnet* | default: empty | The edns0-client-subnet option. | 192.0.2.0/24
+random_padding* | string, ignored | The value of this parameter is ignored. | XmkMw~o_mgP2pf.gpw-Oi5dK
+
+* Parameters with an asterisk are only supported by google.
+* Cloudflare does not allow queries using the "ANY" type, to reduce DNS amplification DDoS attacks.  They are an author on a draft RFC to standardize [refusing ANY](https://tools.ietf.org/html/draft-ietf-dnsop-refuse-any-06) which seems to be a good idea.
+* Google returns the responding server.
+* Google lets you request a specific EDNS client subnet, which might be useful for testing CDNs, etc.  Cloudflare sees this as a privacy risk, my script defaults to 0.0.0.0/32 if you don't specify so there is equal privacy.
+
+[Cloudflare documentation](https://developers.cloudflare.com/1.1.1.1/dns-over-https/json-format/)
+
+[Google Documentation](https://developers.google.com/speed/public-dns/docs/dns-over-https)
+
+### Bugs
+
+* Cloudflare initially had a bug where CAA record answers were not escaped, which meant they were returned as invalid JSON.  They fixed that by the Wednesday after release.
+* Google has a similar escaping issue, they escape only the outermost quotes.  This returns valid JSON, but the answer ends up differnt than what is actually in DNS in cases where the CAA is malformed.  Google accepted the [bug](https://issuetracker.google.com/issues/78002839) but no fix was made.  ¯\\_(ツ)_/¯
+
+Dig:
+
+    bunnyears.com.          299     IN      CAA     0 issue "\"letsencrypt.org\""
+
+Cloudflare:
+
+    bunnyears.com.  257 300 0 issue "\"letsencrypt.org\""
+
+Google:
+
+    bunnyears.com.  257 299 0 issue ""letsencrypt.org""
+
+## Multi-threading and optimizations
+
+One use case I decided on was to do batch queries for CAA records using the Alexa top 1M data set.  This quickly proved to be too slow, but I had been wanting to test mult-threading for a while.  While there are good scripts like invoke-async, I decided to use a jobs pattern from Ryan's [Get-Blog](http://www.get-blog.com/?p=189) and incorporate it directly in my script for customization.  In pounding the script with 50k shards of the top 1m, I found some important optimizations to powershell and the script.  Initially it would take over 18 minutes per shard, with optimizations it takes me between 55-65 seconds, so about 900 queries a second.
+
+1. Appending to an array creates a new array, so the "$Jobs += $Job" line is expensive.
+2. Increased the outbound HTTP [connection limit](https://social.technet.microsoft.com/wiki/contents/articles/29863.powershell-rest-api-invoke-restmethod-gotcha.aspx)
+3. out-null is expensive, instead use "$null = foo" rather than "foo | out-null"
+4. I renamed a $iss for clarity.
+5. foreach-object is faster than where-object, and without progress the disposal loop can be refactored.
+6. Progress display slows things down especially invoke-webrequest.  I put it behind a switch (-Progress) and cleaned up the logic to reduce work.
+7. Customized powershell.exe.config for "server" garbage collection.  My home PC has 32GB of RAM so this might not be for everyone.
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe.config:
+
+        <?xml version="1.0" encoding="utf-8" ?>
+        <configuration>
+            <runtime>
+                <gcServer enabled="true"/>
+                <Thread_UseAllCpuGroups enabled="true"/>
+                <GCCpuGroup enabled="true"/>  
+            </runtime>
+        </configuration>
+
+## Usage
+
+        import-module .\get-dnsoverhttp-mt.ps1
+        Set-Alias doh get-dnsoverhttp-mt
+        doh www.ibm.com
+        get-content biglistofdomains.txt | doh -type CAA -google
+
+I tried to make the parameters a superset of the builtin resolve-dnsname cmdlet.
+
+    NAME
+        get-dnsoverhttp-mt
+
+    SYNTAX
+        get-dnsoverhttp-mt [-Name] <string> [[-Type] <string> {ANY | X | A | AAAA
+        | AFSDB | APL | CAA | CDNSKEY | CDS | CERT | CNAME | DHCID | DLV | DNAME |
+        DNSKEY | DS | HIP | IPSECKEY | KEY | KX | LOC | MX | NAPTR | NS | NSEC |
+        NSEC3 | NSEC3PARAM | OPENPGPKEY | PTR | RP | RRSIG | SIG | SOA | SRV |
+        SSHFP | TA | TKEY | TLSA | TSIG | TXT | URI}] [-Subnet <string>]
+        [-DnssecCd] [-Google] [-Progress] [-MaxThreads <Object>] [-SleepTimer
+        <Object>] [-MaxResultTime <Object>]  [<CommonParameters>]
+
+## Results
+
+* Google DNS is about 20% faster for me.
+* Only about 5800 out of the Alexa1M domains have CAA records - 0.5%
+* Aout the same number out of the Chromium HSTS preload have CAA, but out of 50000, that's about 10%.
---
+++ get-dnsoverhttp-mt.ps1
@@ -0,0 +1,204 @@
+#Requires -version 3
+<#
+.SYNOPSIS
+  resolve-dnsoh.ps
+  # create-alias doh resolve-dnsoverhttp
+  resolve dns query using Cloudflare or Google DNS over HTTPS interface
+.AUTHOR 
+  Brian Howson April 2018 (+credits)
+
+.DESCRIPTION
+
+https://developers.cloudflare.com/1.1.1.1/dns-over-https/json-format/
+
+https://developers.google.com/speed/public-dns/docs/dns-over-https
+
+https://datatracker.ietf.org/doc/draft-hoffman-dns-in-json/
+
+https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-6
+
+https://github.com/curl/curl/wiki/DNS-over-HTTPS
+
+# Multithreaded trickery ( refactored but list.add vs +=, foreach vs. where-object)
+    http://www.get-blog.com/?p=189
+
+TODO: make posh6 portable, enforcing Tls12 on linux
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-restmethod?view=powershell-5.0
+# Connection limit incease trickery from
+    https://social.technet.microsoft.com/wiki/contents/articles/29863.powershell-rest-api-invoke-restmethod-gotcha.aspx
+
+# Check my encoding
+   https://www.reddit.com/r/PowerShell/comments/4k77mo/encoding_spaces_for_invokerestmethod/
+
+# DNS performance (DNS over UDP)
+    https://medium.com/@nykolas.z/dns-resolvers-performance-compared-cloudflare-x-google-x-quad9-x-opendns-149e803734e5
+
+# 
+    https://github.com/PowerShell/PowerShell-Docs/issues/1753
+
+
+# Server Mode Garbage collection.
+-- C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe.config --
+<?xml version="1.0" encoding="utf-8" ?> 
+<configuration>
+    <runtime>
+        <gcServer enabled="true"/>
+        <Thread_UseAllCpuGroups enabled="true"/>
+        <GCCpuGroup enabled="true"/>  
+    </runtime>
+</configuration>
+
+# Ephemeral port exhaustion
+http://www.computertechblog.com/detecting-ephemeral-port-exhaustion-in-windows-7-8-2012/
+
+# test cases:
+    https://httpbin.org/
+
+# Comparison with resolve-dnsname
+
+UNKNOWN, A_AAAA, A, NS, MD, MF, CNAME, SOA, MB, MG, MR, NULL, WKS, PTR, HINFO, MINFO, MX, TXT, RP, AFSDB, X25, ISDN, RT, AAAA,
+SRV, DNAME, OPT, DS, RRSIG, NSEC, DNSKEY, DHCID, NSEC3, NSEC3PARAM, ANY, ALL, WINS"
+
+# Generic list vs. recreating array to add
+   https://stackoverflow.com/questions/14620290/powershell-array-add-vs
+# refactored job dispatch loop, switched where-object to foreach
+
+.Example
+    get-dnsoverhttp report-uri.com 
+    get-dnsoverhttp report-uri.com CAA
+
+    # work like dig -x 104.107.41.53 to get a PTR lookup 53.41.107.104.in-addr.arpa.
+    get-dnsoverhttp 104.107.41.53 X
+
+    import-module get-dnsoverhttp-mt.ps1
+    set-alias 
+
+
+#>
+
+Function get-dnsoverhttp-mt {
+[CmdletBinding()]
+param (
+  [parameter(ValueFromPipeline,Position=0,Mandatory=$true)]
+  [String]$Name,
+  [parameter(ValueFromPipeline,Position=1,Mandatory=$false)]  
+  [ValidateSet("ANY","X","A","AAAA","AFSDB","APL","CAA","CDNSKEY","CDS","CERT","CNAME","DHCID","DLV","DNAME","DNSKEY","DS","HIP","IPSECKEY","KEY","KX","LOC","MX","NAPTR","NS","NSEC","NSEC3","NSEC3PARAM","OPENPGPKEY","PTR","RP","RRSIG","SIG","SOA","SRV","SSHFP","TA","TKEY","TLSA","TSIG","TXT","URI")] 
+  [String]$Type = "A",
+  [string]$Subnet,
+  [switch]$DnssecCd,
+  [switch]$Google,
+  [switch]$Progress,
+  $MaxThreads = 20,    # 20 default, for dns many more
+  $SleepTimer = 200,   # 200ms default, for dns much less
+  $MaxResultTime = 120 # seconds
+)
+BEGIN {
+    $ElapsedTime = [System.Diagnostics.Stopwatch]::StartNew()
+    $CurrentSSL = [System.Net.ServicePointManager]::SecurityProtocol
+    [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $ProgressSaved = $global:ProgressPreference
+    if (-not $Progress) {
+        $global:progressPreference = 'silentlyContinue'
+    }
+    if ($Google -or $DnssecCd -or $Subnet -or $Type -eq "ANY") {
+        write-verbose "Using Google"
+        $uri = "https://dns.google.com/resolve?"
+        if ($DnssecCd) {
+            $uri += "cd=1&"
+        }
+        if ($Subnet) {
+            $uri += "edns_client_subnet=$Subnet&"
+        } else {
+            $uri += "edns_client_subnet=0.0.0.0/0&"
+        }
+        $servicePoint = [System.Net.ServicePointManager]::FindServicePoint("https://dns.google.com")
+    } else {
+        write-verbose "Using Cloudflare"
+        $uri = "https://cloudflare-dns.com/dns-query?ct=application/dns-json&"
+        $servicePoint = [System.Net.ServicePointManager]::FindServicePoint("https://cloudflare-dns.com")
+    }
+
+    $ServicePoint.ConnectionLimit = ($MaxThreads * 2)
+    $global:Session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    $null = invoke-RestMethod -Uri ($uri + "name=google.com&type=A") -UseBasicParsing -headers $headers -UserAgent "" -TimeoutSec 20 -SessionVariable Session
+
+    $scriptblock = {
+        param (
+            [Parameter(mandatory=$false, Position=0)]$uri
+        )
+        $request = [System.Net.WebRequest]::Create($uri)
+        $request.Method="Get"
+        $response = $request.GetResponse()
+        $requestStream = $response.GetResponseStream()
+        $readStream = New-Object System.IO.StreamReader $requestStream
+        $data = $readStream.ReadToEnd()
+        $results = $data | ConvertFrom-Json
+        if ($results.answer) {
+            $results.answer
+        }
+    }
+
+    $InitialSessionState = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+    $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads, $InitialSessionState, $Host)
+    $RunspacePool.Open()
+    $OFS = "`r`n"    # Output Field Separator
+    $Code = [ScriptBlock]::Create($scriptblock)
+    Remove-Variable OFS
+    $Jobs = New-Object System.Collections.Generic.List[System.Object]
+}
+PROCESS {
+    if ($Type -eq "X" -and $Name -match "^(([01]?[0-9]?[0-9]|2[0-5][0-5])\.){3}([01]?[0-9]?[0-9]|2[0-5][0-5])$") {
+        $IP = $Name.split(".")
+        $Name = $IP[3] + "." +  $IP[2] + "." + $IP[1] + "." + $IP[0] + ".in-addr.arpa."
+        $Type = "PTR"
+    }
+    ForEach ($nm in $Name){
+        $quri = $uri + "name=$nm&type=$Type"
+        write-verbose "$quri"
+        if ($Progress) {
+            Write-Progress -Activity "Preloading threads" -Status "Starting Job $($jobs.count) $Name"
+        }
+        $PowershellThread = [powershell]::Create().AddScript($Code)
+        $null = $PowershellThread.AddParameter("uri",$quri)
+        $PowershellThread.RunspacePool = $RunspacePool
+        $Handle = $PowershellThread.BeginInvoke()
+
+        $Job = "" | Select-Object Handle, Thread, object
+        $Job.Handle = $Handle
+        $Job.Thread = $PowershellThread
+        $Job.Object = $Name
+        $Jobs.Add($Job)
+    }
+}
+End {
+    $Remaining = 1
+#    $maxrun = $($MaxThreads - $($RunspacePool.GetAvailableRunspaces()))
+    While ($Remaining -gt 0)  {        
+        $Remaining = 0
+        ForEach ($Job in $Jobs) {
+            if ($Job.Handle.IsCompleted -eq $True) {
+                $Job.Thread.EndInvoke($Job.Handle)
+                $Job.Thread.Dispose()
+                $Job.Thread = $Null
+                $Job.Handle = $Null
+            } elseif ((-not $running) -and ($Job.Handle -ne $Null)) {
+                $Remaining += 1
+            }                
+        }
+        if ($Progress) {
+            Write-Progress `
+                -Activity "Waiting for Jobs - $($MaxThreads - $($RunspacePool.GetAvailableRunspaces())) of $MaxThreads threads running" `
+                -PercentComplete (($Jobs.Count - $Remaining)/ $Jobs.Count * 100)`
+                -Status "Remaining $Remaining"
+        }
+        Start-Sleep -Milliseconds $SleepTimer
+    } 
+    $null = $RunspacePool.Close()
+    $null = $RunspacePool.Dispose()
+    $null = $ServicePoint.CloseConnectionGroup("")
+    [System.Net.ServicePointManager]::SecurityProtocol = $CurrentSSL
+    $global:ProgressPreference = $ProgressSaved
+    write-verbose "Elapsed Time: $($ElapsedTime.Elapsed.ToString())"
+    write-host "Elapsed Time: $($ElapsedTime.Elapsed.ToString())"
+}
+}
